a9139230b004c0a8315afb6132cff9a4d84ad3e9
Mien Khai Giang <144539514+giangkhai@users.noreply.github.com>
2024-10-07 18:14:42 +0700

    Update CNN_model.py

--- CNN_LS_estimation/CNN_model.py
+++ CNN_LS_estimation/CNN_model.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models, initializers
-
 def create_model(learning_rate):
-    inputs = layers.Input(shape=(128, 1))  # Input: H_LS = [Re(H_LS) Im(H_LS)]
+    inputs = layers.Input(shape=(2*K, 1))  
+
 
     x = inputs
     for i in range(10):
@@ -10,21 +10,23 @@ def create_model(learning_rate):
         x = layers.BatchNormalization()(x)
         x = layers.Dropout(0.5)(x)
 
+ 
     x = layers.Flatten()(x)
 
-
-    output_layer = layers.Dense(128, activation='linear', use_bias=False, 
-                                kernel_initializer=initializers.Identity())(x)
-    output_layer = layers.Reshape((128, 1))(output_layer)
+   
+    output_layer = layers.Dense(2*K, activation='linear', use_bias=False,
+                                kernel_initializer='glorot_uniform')(x)
+    output_layer = layers.Reshape((2*K, 1))(output_layer)
 
     model = models.Model(inputs=inputs, outputs=output_layer, name="cnn_model")
-    
- 
-    #model.summary()
 
- 
+
+    model.summary()
+
     optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
     model.compile(optimizer=optimizer, loss='mean_squared_error')
 
     return model
+
+
 cnn_model = create_model(learning_rate=1e-3 / 2)
